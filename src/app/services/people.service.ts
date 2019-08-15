4ca89eddaedc14b552be03be2ffd93f42c430ea1
dotCero <lucero.inginfo@gmail.com>
2019-08-14 20:50:15 -0400

    Cambiando de .list() a .object() (w10)

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,31 +1,51 @@
 import {Injectable} from '@angular/core';
-import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import {AngularFireDatabase, AngularFireList, AngularFireObject} from '@angular/fire/database';
 import {Observable} from 'rxjs';
 import {People} from '../model/people';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
-  private resultset: AngularFireList<People>;
-  private peopless: [];
+  private resultset: AngularFireObject<People>;
+  public obs: Observable<People>;
+  private peopless: any;
   public hola: string;
 
   constructor(database: AngularFireDatabase) {
-    this.resultset = database.list('people');
+    this.resultset = database.object('people');
+
+    /*this.obs = this.resultset.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );*/
+    this.obs = this.resultset.valueChanges();
   }
 
   public getPeoples() {
     /*return this.resultset.valueChanges();*/
-    this.resultset.snapshotChanges().subscribe(peoples => {
+    /*this.resultset.snapshotChanges().subscribe(peoples => {
       peoples.forEach(p => {
-        const people = new People();
-        people.name = p.payload.val().name;
-        people.photo = p.payload.val().photo;
-        people.position = p.payload.val().position;
-        this.peopless.push(people);
+        console.log(p.type);
+        console.log(p.key);
+        console.log(p.payload.val());
       });
     });
-    return this.peopless;
+    return this.peopless;*/
   }
+
+  /*addItem(newName: string) {
+    this.resultset.push({lastnames: {one: newName, two: newName}, photo: newName, position: 25, name: newName});
+  }
+  updateItem(key: string, newText: string) {
+    this.resultset.update(key, {lastnames: {one: newText, two: newText}, photo: newText, position: 25, name: newText});
+  }
+  deleteItem(key: string) {
+    this.resultset.remove(key);
+  }
+  deleteEverything() {
+    this.resultset.remove();
+  }*/
 }
